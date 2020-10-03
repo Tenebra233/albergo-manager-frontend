3e4ee6a9703c41d7b09b99bc1a4c5e23408a3cc7
roberto.urbani <thetenebrax23@gmail.com>
2020-10-03 20:19:50 +0200

    Integrazione API

--- FormGenerator.php
+++ FormGenerator.php
@@ -4,143 +4,80 @@ require_once('/var/www/html/albergo-wp/SwaggerClient-php/vendor/autoload.php');
 
 class FormGenerator
 {
-    private $servername = "localhost";
-    private $username = "root";
-    private $password = "pippo";
-    private $dbname = "nova-test";
+    //    private $servername = "localhost";
+    //    private $username = "root";
+    //    private $password = "pippo";
+    //    private $dbname = "nova-test";
     private $form;
-
+    private $name;
+    private $email;
+    private $password;
+    private $note;
+    
     public static function Hooks()
     {
         $me = new self();
         require_once '/var/www/html/albergo-wp/vendor/formr/formr/class.formr.php';
-
+        
         add_action('wp_enqueue_scripts', [$me, 'your_theme_enqueue_scripts']);
         add_action('wp_head', [$me, 'sendInfo']);
         add_shortcode('form-test', [$me, 'form_test']);
         $me->form = new Formr('bootstrap');
-
-
+        
+        
     }
-
+    
     public function your_theme_enqueue_scripts()
     {
         // all styles
         wp_enqueue_style('bootstrap', '/css/bootstrap.css', array(), 20141119);
         // all scripts
-        wp_enqueue_script('bootstrap', '/js/bootstrap.min.js', array('jquery'), '20120206', true);
+        wp_enqueue_script('bootstrap', '/js/bootstrap.min.js', array('jquery'), '20120206', TRUE);
     }
-
+    
     public function form_test()
-
-
+    
+    
     {
-
-        $form = new Formr();
-        echo $form->form_open();
-        echo $form->input_text('name', 'Nome');
-        echo $form->input_email('email', 'Email');
-        echo $form->input_password('password', 'Password');
-        echo $form->input_textarea('note', 'Note');
-        echo $form->input_submit();
-
+        
+        ob_start();
+        echo $this->form->form_open();
+        echo $this->form->input_text('name', 'Nome');
+        echo $this->form->input_email('email', 'Email');
+        echo $this->form->input_password('password', 'Password');
+        echo $this->form->input_textarea('note', 'Note');
+        echo $this->form->input_submit();
+        $content = ob_get_contents();
+        ob_end_clean();
+        return $content;
+        
     }
-
+    
     public function sendInfo()
     {
-
-
+        
+        
         if ($this->form->submitted()) {
-            $test = new Swagger\Client\Api\RegisterApi();
-            $body = new \Swagger\Client\Model\Body2([
-                'id'=>666,
-                'name'=>'swaggsdetest',
-                'email'=>'ajfioajof@sdgmaasd.com',
-                'password'=>'aajioajofija123dd'
+            $test = new \Swagger\Client\Api\FlussoPrenotazioneApi();
+            $body = new \Swagger\Client\Model\Body1([
+                'name' => $this->form->post('name'),
+                'email' => $this->form->post('email'),
+                'password' => $this->form->post('password'),
+                "data" => "2020-09-28 10:00:00",
+                "aliquota_iva" => "22",
+                "totale" => "1000",
+                "data_prenotazione" => "2020-09-28 10:00:00",
+                "note" => $this->form->post('note'),
+                "status" => "S",
             ]);
-            $test->registerUserWithHttpInfo($body);
-
+            $test->flussoPrenotazione($body);
+            
             $name = $this->form->post('name');
             $email = $this->form->post('email');
             $password = $this->form->post('password');
             $note = $this->form->post('note');
-
-
-            //FLUSSO 1
-//            $createUser = curl_init();
-//
-//            curl_setopt_array($createUser, array(
-//                CURLOPT_URL => "http://laravel.dev/api/users",
-//                CURLOPT_RETURNTRANSFER => true,
-//                CURLOPT_ENCODING => "",
-//                CURLOPT_MAXREDIRS => 10,
-//                CURLOPT_TIMEOUT => 0,
-//                CURLOPT_FOLLOWLOCATION => true,
-//                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-//                CURLOPT_CUSTOMREQUEST => "POST",
-//                CURLOPT_POSTFIELDS => "name=$name&email=$email&password=$password",
-//                CURLOPT_HTTPHEADER => array(
-//                    "Content-Type: application/x-www-form-urlencoded"
-//                ),
-//            ));
-//
-//
-//            $userResponse = curl_exec($createUser);
-//            $userJson = json_decode($userResponse, true);
-//            $userId = $userJson['id'];
-//
-//            //FLUSSO 2
-//
-//            $createFattura = curl_init();
-//
-//            curl_setopt_array($createFattura, array(
-//                CURLOPT_URL => "http://laravel.dev/api/fattura",
-//                CURLOPT_RETURNTRANSFER => true,
-//                CURLOPT_ENCODING => "",
-//                CURLOPT_MAXREDIRS => 10,
-//                CURLOPT_TIMEOUT => 0,
-//                CURLOPT_FOLLOWLOCATION => true,
-//                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-//                CURLOPT_CUSTOMREQUEST => "POST",
-//                CURLOPT_POSTFIELDS => "data=2020-09-02 12:00:00&aliquota_iva=22&totale=100&user_id=$userId",
-//                CURLOPT_HTTPHEADER => array(
-//                    "Content-Type: application/x-www-form-urlencoded"
-//                ),
-//            ));
-//
-//
-//            $fatturaResponse = curl_exec($createFattura);
-//            $fatturaJson = json_decode($fatturaResponse, true);
-//            $fatturaId = $fatturaJson['id'];
-//
-//            //FLUSSO 3
-//            $createPrenotazione = curl_init();
-//
-//            curl_setopt_array($createPrenotazione, array(
-//                CURLOPT_URL => "http://laravel.dev/api/prenotazione",
-//                CURLOPT_RETURNTRANSFER => true,
-//                CURLOPT_ENCODING => "",
-//                CURLOPT_MAXREDIRS => 10,
-//                CURLOPT_TIMEOUT => 0,
-//                CURLOPT_FOLLOWLOCATION => true,
-//                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-//                CURLOPT_CUSTOMREQUEST => "POST",
-//                CURLOPT_POSTFIELDS => "data_prenotazione=" . date('Y-m-d') . "&note=$note&status=S&fattura_id=$fatturaId",
-//                CURLOPT_HTTPHEADER => array(
-//                    "Content-Type: application/x-www-form-urlencoded"
-//                ),
-//            ));
-//
-//
-//            $test = curl_exec($createPrenotazione);
-//
-//            curl_close($createPrenotazione);
-//            curl_close($createFattura);
-//            curl_close($createUser);
-
-
+            
         }
     }
-
-
+    
 }
