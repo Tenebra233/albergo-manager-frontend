346cc594715ae383ad208a78854ce33bdf68fcde
roberto.urbani <thetenebrax23@gmail.com>
2020-10-04 18:56:32 +0200

    Aggiunta hardcoded data_a e data_a per le prenotazioni

--- FormGenerator.php
+++ FormGenerator.php
@@ -14,20 +14,20 @@ class FormGenerator
     private $email;
     private $password;
     private $note;
-    
+
     public static function Hooks()
     {
         $me = new self();
         require_once '/var/www/html/albergo-wp/vendor/formr/formr/class.formr.php';
-        
+
         add_action('wp_enqueue_scripts', [$me, 'your_theme_enqueue_scripts']);
         add_action('wp_head', [$me, 'sendInfo']);
         add_shortcode('form-test', [$me, 'form_test']);
         $me->form = new Formr('bootstrap');
-        
-        
+
+
     }
-    
+
     public function your_theme_enqueue_scripts()
     {
         // all styles
@@ -35,36 +35,40 @@ class FormGenerator
         // all scripts
         wp_enqueue_script('bootstrap', '/js/bootstrap.min.js', array('jquery'), '20120206', TRUE);
     }
-    
+
     public function form_test()
-    
-    
+
+
     {
-        
+
         ob_start();
         echo $this->form->form_open();
         echo $this->form->input_text('name', 'Nome');
         echo $this->form->input_email('email', 'Email');
         echo $this->form->input_password('password', 'Password');
+        echo $this->form->input_datetime_local('data_da', 'Dal');
+        echo $this->form->input_datetime_local('data_a', 'Al');
         echo $this->form->input_textarea('note', 'Note');
         echo $this->form->input_submit();
         $content = ob_get_contents();
         ob_end_clean();
         return $content;
-        
+
     }
-    
+
     public function sendInfo()
     {
-        
-        
+
+
         if ($this->form->submitted()) {
             $test = new \Swagger\Client\Api\FlussoPrenotazioneApi();
             $body = new \Swagger\Client\Model\Body1([
                 'name' => $this->form->post('name'),
                 'email' => $this->form->post('email'),
                 'password' => $this->form->post('password'),
-                "data" => "2020-09-28 10:00:00",
+                'data' => "2020-09-28 10:00:00",
+                "data_da" => "2020-09-28 10:00:00",
+                "data_a" => "2020-09-28 10:00:00",
                 "aliquota_iva" => "22",
                 "totale" => "1000",
                 "data_prenotazione" => "2020-09-28 10:00:00",
@@ -72,13 +76,8 @@ class FormGenerator
                 "status" => "S",
             ]);
             $test->flussoPrenotazione($body);
-            
-            $name = $this->form->post('name');
-            $email = $this->form->post('email');
-            $password = $this->form->post('password');
-            $note = $this->form->post('note');
-            
+
         }
     }
-    
+
 }
